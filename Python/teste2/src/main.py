2924afa55bdb4caeececc50e48661a532cdbcc89
RodrigoGCorrea <rodrigogc@id.uff.br>
2021-06-11 13:20:17 -0300

    importando arquivos para zip

--- Python/teste2/src/main.py
+++ Python/teste2/src/main.py
@@ -1,13 +1,17 @@
+# encoding: utf-8
 from parse import *
 from tabula import read_pdf
 from pdfrw import PdfReader, PdfWriter
 from pandas import to_numeric, concat
+from zipfile import ZipFile, ZIP_DEFLATED
 import os
 
 
 def main():
     # Cortei o pdf para conter somente a parte em que queremos tirar os dados para facilitar o processamento
     file = "../Componente Organizacional.pdf"
+    listFiles = ["Quadro 30", "Quadro 31", "Quadro 32"]
+    dfList = ["", "", ""]
 
     pages = PdfReader(file).pages
     crop = (79, 86)
@@ -20,10 +24,7 @@ def main():
 
     # passei o pdf no tabula para gerar as tabelas dentro do python
     tables = read_pdf(croppedFile, pages="all")
-
-    # os quadros 30 e 32 são parecidos, facilitando o processamento
-    df30 = ParseDFSimple(tables[0])
-    df32 = ParseDFSimple(tables[-1])
+    print("PDF foi cortado")
 
     # organizei o começo do quadro 31
     df31 = tables[1]
@@ -37,13 +38,31 @@ def main():
     )
     df31 = df31.drop([0])
 
+    # arrumei o quadro 31, que veio cortado devido a estar em mais páginas
     for i in range(2, len(tables)-1):
         df31middle = ParseDFMiddle(tables[i], rowNames)
         df31 = concat([df31, df31middle], ignore_index=True)
     
     df31[df31.columns[0]] = to_numeric(df31[df31.columns[0]])
-    df31 = df31.sort_values(df31.columns[0])
-    print(df31)
+    dfList[1] = df31.sort_values(df31.columns[0])
+    print(listFiles[1] + " construído")
 
+    # os quadros 30 e 32 são parecidos, facilitando o processamento
+    dfList[0] = ParseDFSimple(tables[0])
+    print(listFiles[0]+" construído")
+    dfList[2] = ParseDFSimple(tables[-1])
+    print(listFiles[2] + " construído")
 
+    # guardei os arquivos CSV
+    for i in range(3):
+        dfList[i].to_csv(listFiles[i] + ".csv", index=False, encoding='utf-8')
+    print("CSVs construídos")
+
+    # colocando os arquivos em um zip
+    with ZipFile('Teste_Intuitive_Care_Rodrigo_Correa.zip', 'w') as zipF:
+        for file in listFiles:
+            zipF.write(file+".csv", compress_type=ZIP_DEFLATED)
+            os.remove(file + ".csv")
+    print("Zip construido em "+os.getcwd())
+    
 main()
