039b1b06fcd49e09929a1acfcf80d7864f8e3673
RodrigoGCorrea <rodrigogc@id.uff.br>
2021-06-15 18:21:46 -0300

    added bootstrap and some visual changes

--- Vue/teste4/src/components/Search.vue
+++ Vue/teste4/src/components/Search.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="app">
   <div class="container">
     <input v-model="searching" placeholder="Digite o que quer pesquisar" />
 
@@ -25,40 +25,44 @@
       <option value="DataRegistroANS">DataRegistroANS</option>
     </select>
 
-    <button @click="getData">Pesquisar</button>
+    <button @click="getData" class="btn btn-secondary">Pesquisar</button>
     </div>
-    <div v-if="answer === 'vazio'">Pesquise algo</div>
-    <div v-else-if="answer === []">Nenhum elemento encontrado</div>
-    <div v-else class="container">
-      <table>
-        <tr>
-          <th>RegistroANS</th>
-          <th>CNPJ</th>
-          <th>RazaoSocial</th>
-          <th>NomeFantasia</th>
-          <th>Modalidade</th>
-          <th>Logradouro</th>
-          <th>Numero</th>
-          <th>Complemento</th>
-          <th>Bairro</th>
-          <th>Cidade</th>
-          <th>UF</th>
-          <th>CEP</th>
-          <th>DDD</th>
-          <th>Telefone</th>
-          <th>Fax</th>
-          <th>EnderecoEletronico</th>
-          <th>Representante</th>
-          <th>CargoRepresentante</th>
-          <th>DataRegistroANS</th>
-        </tr>
-        <tr v-for="row of answer" :key="row.id">
-          <td v-for="element of row" :key="element.id">
-            {{element}}
-          </td>
-        </tr>
+    <div v-if="answer === 'start'"></div>
+    <div v-else-if="answer.length > 0" class="table table-responsive mw-80">
+      <table class="table table-striped table-bordered">
+        <thead class="thead">
+          <tr>
+            <th>RegistroANS</th>
+            <th>CNPJ</th>
+            <th>RazaoSocial</th>
+            <th>NomeFantasia</th>
+            <th>Modalidade</th>
+            <th>Logradouro</th>
+            <th>Numero</th>
+            <th>Complemento</th>
+            <th>Bairro</th>
+            <th>Cidade</th>
+            <th>UF</th>
+            <th>CEP</th>
+            <th>DDD</th>
+            <th>Telefone</th>
+            <th>Fax</th>
+            <th>EnderecoEletronico</th>
+            <th>Representante</th>
+            <th>CargoRepresentante</th>
+            <th>DataRegistroANS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row of answer" :key="row.id" >
+            <td v-for="element of row" :key="element.id" >
+              {{element}}
+            </td>
+          </tr>
+        </tbody>
       </table>
   </div>
+  <div v-else>Nenhum item encontrado</div>
 </div>
 </template> 
 
@@ -70,7 +74,7 @@ export default {
     return {
       searching: '',
       column: "RegistroANS",
-      answer: 'vazio'
+      answer: 'start'
     }
   },
   methods: {
@@ -80,9 +84,6 @@ export default {
         url: `http://localhost:5000?${this.column}=${this.searching}`,
       }).then((res) => {
         this.answer=res.data;
-        if (this.answer == []) {
-          console.log("alou");
-        }
       })
     }
   }
@@ -91,13 +92,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.app {
+  justify-content: center;
+}
+
 .container {
   display: flex;
   justify-content: center;
   gap: 10px;
 }
 input {
-  font-size: 1.5em;
+  font-size: 1.2em;
   width: 90vh;
 }
+.table {
+  justify-content: start;
+  font-size: 0.7rem;
+  margin-left: 1%;
+  margin-top: 1%;
+  width: 98%;
+}
+
 </style>
